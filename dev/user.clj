e0bb7013f54f10b804ab2f3b2c5143f038f13659
Hunter Husar <hunter@hunterhusar.net>
2016-10-17 22:17:54 -0500

    wip

--- dev/user.clj
+++ dev/user.clj
@@ -3,8 +3,8 @@
             [clojure.string :as str]
             [clojure.pprint :refer (pprint)]
             [clojure.repl :refer :all]
-            [clojure.test :as test]
             [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+            [com.stuartsierra.component :as component]
             [mini-lord.system :as system]))
 
 (def system nil)
@@ -18,13 +18,13 @@
 (defn start
   "Starts the current development system."
   []
-  (alter-var-root #'system system/start))
+  (alter-var-root #'system component/start))
 
 (defn stop
   "Shuts down and destroys the current development system."
   []
   (alter-var-root #'system
-    (fn [s] (when s (system/stop s)))))
+    (fn [s] (when s (component/stop s)))))
 
 (defn go
   "Initializes the current development system and starts it running."
--- src/mini_lord/system.clj
+++ src/mini_lord/system.clj
@@ -1,21 +1,26 @@
 (ns mini-lord.system
-  (:require [ring.adapter.jetty :as jetty]
-            [com.stuartsierra.component :as component]))
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
 
 (defn system
   "returns a new instance of the whole app"
   []
-  (println "foo"))
-
+  {:handler handler
+   :server {:port 3000}})
 
 (defn start
   "Performs side effects to initialize the system, acquire resources,
   and start it running. Returns an updated instance of the system."
   [system]
-  (println "start"))
+  (jetty/run-jetty handler (:server system)))
 
 (defn stop
   "Performs side effects to shut down the system and release its
   resources. Returns an updated instance of the system."
   [system]
   (println "stop"))
+
